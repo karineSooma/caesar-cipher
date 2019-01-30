b5cd0405f2540fd3fffd16aa5c6263f2934f3f71
soomaNine <karinesooma5411@gmail.comgit config --global user.name soomaNinegit commit -m testegit statusgit log --onlinegit statusgit pushgit config --global user.email karinesooma5411@gmail.com>
2019-01-30 14:59:36 -0200

    atualizado

--- src/cipher.js
+++ src/cipher.js
@@ -13,19 +13,17 @@ function descripto() {
 }
 
 //Função para cifrar mensagem
-function cipher (offset, message) {
-    let resultMessage = "", code;
+function cipher(offset, message) {
+    let resultMessage = "";
     for (let i = 0; i < message.length; i++) {
-      if (message.charCodeAt(i) >= 65 && message.charCodeAt(i) <= 90) {
-        code = (((message.charCodeAt(i) - 65) + offset) % 26) + 65;
-      }
-      else if (message.charCodeAt(i) >= 97 && message.charCodeAt(i) <= 122) {
-        code = (((message.charCodeAt(i) - 97) + offset) % 26) + 97;
-      }
-      else if (message.charCodeAt(i) === 32) {
-        code = 32;
-      }
-      resultMessage += String.fromCharCode(code);
+        if (message.charCodeAt(i) >= 65 && message.charCodeAt(i) <= 90) {
+            code = (((message.charCodeAt(i) - 65) + offset) % 26) + 65;
+        } else if (message.charCodeAt(i) >= 97 && message.charCodeAt(i) <= 122) {
+            code = (((message.charCodeAt(i) - 97) + offset) % 26) + 97;
+        } else if (message.charCodeAt(i) === 32) {
+            code = 32;
+        }
+        resultMessage += String.fromCharCode(code);
     }
     return resultMessage;
 }
