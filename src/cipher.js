0d9d919f770eebfa1d223ec59d8ce69b03c0a797
soomaNine <karinesooma5411@gmail.comgit config --global user.name soomaNinegit commit -m testegit statusgit log --onlinegit statusgit pushgit config --global user.email karinesooma5411@gmail.com>
2019-01-30 15:58:00 -0200

    funcoes feitas e funcionando

--- src/cipher.js
+++ src/cipher.js
@@ -26,4 +26,32 @@ function cipher(offset, message) {
         resultMessage += String.fromCharCode(code);
     }
     return resultMessage;
+}function cipher(offset, message) {
+    let resultMessage = "";
+    for (let i = 0; i < message.length; i++) {
+        if (message.charCodeAt(i) >= 65 && message.charCodeAt(i) <= 90) {
+            code = (((message.charCodeAt(i) - 65) + offset) % 26) + 65;
+        } else if (message.charCodeAt(i) >= 97 && message.charCodeAt(i) <= 122) {
+            code = (((message.charCodeAt(i) - 97) + offset) % 26) + 97;
+        } else if (message.charCodeAt(i) === 32) {
+            code = 32;
+        }
+        resultMessage += String.fromCharCode(code);
+    }
+    return resultMessage;
+}
+//função para descifrar mensagem
+function decipher(offset, message) {
+    let resultMessage = "";
+    for (let i = 0; i < message.length; i++) {
+        if (message.charCodeAt(i) >= 65 && message.charCodeAt(i) <= 90) {
+            code = (((message.charCodeAt(i) - 65) - offset) % 26) + 65;
+        } else if (message.charCodeAt(i) >= 97 && message.charCodeAt(i) <= 122) {
+            code = (((message.charCodeAt(i) - 97) - offset) % 26) + 97;
+        } else if (message.charCodeAt(i) === 32) {
+            code = 32;
+        }
+        resultMessage += String.fromCharCode(code);
+    }
+    return resultMessage;
 }
