160a5afa00734fa3b06a59965683f8cd23f1dff5
soomaNine <karinesooma5411@gmail.comgit config --global user.name soomaNinegit commit -m testegit statusgit log --onlinegit statusgit pushgit config --global user.email karinesooma5411@gmail.com>
2019-01-30 13:18:43 -0200

    fiz funcao cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,12 +1,31 @@
-function buttonStart (){
-let chooseOption= parseInt(prompt("Escolha uma alternativa: \n 1. Criptografar \n 2. Descriptografar"));
-    if (chooseOption===1){
-    cipher ();
-    } 
-    else if (chooseOption===2){
-    decipher();
-    }
-    return alert("Por favor, selecione uma opção para continuar");
-    
+function cripto() {
+    let message = document.getElementById("enterText").value;
+    let offset = parseInt(document.getElementById("enterOffset").value);
+    let criptoMessage = cipher(offset, message);
+    document.getElementById("resultAnswerCripto").innerHTML = criptoMessage;
 }
-console.log (chooseOption);
+
+function descripto() {
+    let message = document.getElementById("enterText").value;
+    let offset = parseInt(document.getElementById("enterOffset").value);
+    let descriptoMessage = decipher(offset, message);
+    document.getElementById("resultAnswerDecripto").innerHTML = descriptoMessage;
+}
+
+//Função para cifrar mensagem
+function cipher (offset, message) {
+    let resultMessage = "", code;
+    for (let i = 0; i < message.length; i++) {
+      if (message.charCodeAt(i) >= 65 && message.charCodeAt(i) <= 90) {
+        code = (((message.charCodeAt(i) - 65) + offset) % 26) + 65;
+      }
+      else if (message.charCodeAt(i) >= 97 && message.charCodeAt(i) <= 122) {
+        code = (((message.charCodeAt(i) - 97) + offset) % 26) + 97;
+      }
+      else if (message.charCodeAt(i) === 32) {
+        code = 32;
+      }
+      resultMessage += String.fromCharCode(code);
+    }
+    return resultMessage;
+}
